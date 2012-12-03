e410e0c22c23fc9b9e16d09bffe8c800e70738d5
James Baiera <jbaiera@kent.edu>
2012-12-03 17:28:58 -0500

    Adding documentation and Entity Parents

--- src/com/stonetolb/engine/Entity.java
+++ src/com/stonetolb/engine/Entity.java
@@ -31,6 +31,17 @@ import com.stonetolb.util.Pair;
  * They are composed of different concrete {@link EntityComponent} 
  * that change it's behavior. They characteristically have only one 
  * {@link RenderComponent} which defines how they are rendered.
+ * <p>
+ * Entities may have parent Entities. This creates a locational dependancy only.
+ * Entities will continue to display their coordinates as a displacement from the origin, 
+ * but instead of using the true origin of the screen, Entities that have parents will 
+ * treat their parent's position as their origin. This leads to two possible Entity locations: <br><br>
+ * 1. Relative Location : The Entity's relative distance from it's origin <br>
+ * 2. Absolute Location : The Entity's distance from the world origin <br>
+ * <p>
+ * Care should be taken when using an Entity's location. Relative Location is Read - Write, but
+ * Absolute Location is Read Only. Entities should only ever update their Relative Location.
+ * 
  * 
  * @author comet
  *
@@ -39,22 +50,52 @@ public class Entity {
 	private String id;
 	
 	protected Pair<Float, Float> position;
+	protected Pair<Float, Float> absolute;
 	protected float direction;
 	protected int speed;
+	protected Entity sceneParent;
 	
 	protected RenderComponent renderComponent;
 	
 	protected Map<String,EntityComponent> components;
 	
-	public Entity(String pId) {
+	/**
+	 * Creates Entity with specified Parent Entity
+	 * 
+	 * @param pId Identifier for Entity Object. Does not need to be unique, but it is advised
+	 * @param pSceneParent Parent of the Entity Object.
+	 */
+	public Entity(String pId, Entity pSceneParent) {
 		id = pId;
 		
 		position = new Pair<Float, Float>(0.0f, 0.0f);
 		direction = 0;
+		speed = 0;
+		
+		sceneParent = pSceneParent;
+		
+		updateAbsoluteLocation();
 		
 		components = new HashMap<String,EntityComponent>();
 	}
 	
+	/**
+	 * Creates Entity with no Parent
+	 * 
+	 * @param pId
+	 */
+	public Entity(String pId)
+	{
+		this(pId, null);
+	}
+	
+	/**
+	 * Adds the given component to the Entity. Due to the underlying implementation,
+	 * The order that the objects have been added in has no bearing on which order
+	 * they are updated.
+	 * 
+	 * @param pNewComponent
+	 */
 	public void addComponent(EntityComponent pNewComponent) {
 		if (RenderComponent.class.isInstance(pNewComponent)) {
 			renderComponent = (RenderComponent) pNewComponent;
@@ -76,6 +117,10 @@ public class Entity {
 		return position;
 	}
 	
+	public Pair<Float, Float> getAbsolute() {
+		return absolute;
+	}
+	
 	public float getDirection() {
 		return direction;
 	}
@@ -86,6 +131,7 @@ public class Entity {
 	
 	public void setPosition(Pair<Float, Float> pPosition) {
 		position = pPosition;
+		updateAbsoluteLocation();
 	}
 	
 	public void setDirection(float pDirection) {
@@ -96,6 +142,10 @@ public class Entity {
 		speed = pSpeed;
 	}
 	
+	public Entity getParent() {
+		return sceneParent;
+	}
+	
 	/**
 	 * Called to update the Entity Object's state based on
 	 * the components the entity is built out of.
@@ -103,6 +153,7 @@ public class Entity {
 	 * @param delta
 	 */
 	public void update(long delta) {
+		updateAbsoluteLocation();
 		EntityComponent current;
 		for(String key : components.keySet()) {
 			current = components.get(key);
@@ -120,6 +171,15 @@ public class Entity {
 	public void render(long delta) {
 		if (renderComponent != null) {
 			renderComponent.render(delta);
+		}
+	}
+	
+	private void updateAbsoluteLocation() {
+		// Get the Parent's absolute location and add your own position to it
+		if (sceneParent != null) {
+			Pair<Float, Float> parentLocation = sceneParent.getAbsolute();
+			absolute.x = parentLocation.x + position.x;
+			absolute.y = parentLocation.y + position.y;
 		}
 	}
 }
